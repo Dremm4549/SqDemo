952f1022073f410675f5f1417415c2f5d48f4b34
Zolmad <50328404+Zolmad@users.noreply.github.com>
2021-12-05 21:21:50 -0500

    Add files via upload

---
+++ Methodical_group12/Methodical_group12/Methodical_group12/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodical_group12
+{
+
+    /** \class Invoice
+    \brief Class to model Invoice in the system. 
+
+    This Invoice Class will represent all the attributes needed to create an Invoice
+    for an Order
+    */
+    public class Invoice
+    {
+        int InvoiceID { set; get; }
+        int BuyerID { set; get; }
+        string BuyerSurName { set; get; }
+        string BuyerLastName { set; get; }
+        int CustomerID { set; get; }
+        string CustomerSurName { set; get; }
+        string CustomerLastName { set; get; }
+        int OrderId { set; get; }
+        string OrderAddress { set; get; }
+    }
+}
